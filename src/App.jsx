6f86c186b0a8d8d0bf34b822b8c9fe75809fd5a5
NesanThivyan <thivyanesan25@gmail.com>
2025-04-27 21:09:38 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 // import './App.css'
 import Parent from './components/parent'
 import Child from './components/child'
